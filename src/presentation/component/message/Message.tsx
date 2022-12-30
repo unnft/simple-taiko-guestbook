e94b9d6ebb99921c23729328ab7bc179595b8eae
unnft <110309533+unnft@users.noreply.github.com>
2022-12-30 04:07:53 +0100

    Date formatting (this needs a useable lib)

--- src/presentation/component/message/Message.tsx
+++ src/presentation/component/message/Message.tsx
@@ -8,7 +8,31 @@ export const Message = ({
 }: {
   message: MessageData;
   index: number;
-}) => {
+}) => (
+  <div
+    key={`msg${index}`}
+    className={`Message ${message.timestamp ? "" : "Message-Pending"}`}
+  >
+    <span>
+      <Jazzicon diameter={45} seed={jsNumberForAddress(message.sender)} />
+    </span>
+    <div>
+      <span className="sender">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={`https://l2explorer.a1.taiko.xyz/address/${message.sender}`}
+        >
+          {message.sender}
+        </a>{" "}
+        <em>{getDateString(message)}</em>
+      </span>
+      <span className="message">{message.msg}</span>
+    </div>
+  </div>
+);
+
+function getDateString(message: MessageData) {
   const dateNow = new Date();
 
   const messageBlockDate = message.timestamp
@@ -17,32 +41,12 @@ export const Message = ({
 
   const dateString = !messageBlockDate
     ? "pending"
-    : messageBlockDate.getDay() !== dateNow.getDay()
+    : messageBlockDate.getTime() + 86400000 < dateNow.getTime()
     ? `${messageBlockDate.toLocaleDateString()}@${messageBlockDate.toLocaleTimeString()}`
     : messageBlockDate.getHours() === dateNow.getHours()
     ? `${dateNow.getMinutes() - messageBlockDate.getMinutes()}min ago`
-    : `${
-        dateNow.getHours() - messageBlockDate.getHours()
-      }h${messageBlockDate.getMinutes()}min ago`;
-
-  return (
-    <div key={`msg${index}`} className={`Message ${message.timestamp ? "" : "Message-Pending"}`}>
-      <span>
-        <Jazzicon diameter={45} seed={jsNumberForAddress(message.sender)} />
-      </span>
-      <div>
-        <span className="sender">
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={`https://l2explorer.a1.taiko.xyz/address/${message.sender}`}
-          >
-            {message.sender}
-          </a>{" "}
-          <em>{dateString}</em>
-        </span>
-        <span className="message">{message.msg}</span>
-      </div>
-    </div>
-  );
-};
+    : `${Math.floor(
+        (dateNow.getTime() - messageBlockDate.getTime()) / 3600000
+      )}h${messageBlockDate.getMinutes()}min ago`;
+  return dateString;
+}
